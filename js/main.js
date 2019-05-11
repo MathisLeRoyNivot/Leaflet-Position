5684428a0169e266ca6c0c5e2c015c24ad58ebcb
Mathis Le Roy-Nivot <mathis.lrn061198@gmail.com>
2019-05-11 17:51:18 +0200

    Starting with jquery

--- js/main.js
+++ js/main.js
@@ -1,19 +1,34 @@
 let longitude = 51.5;
 let lattitude = -0.09;
 
+let longitude1 = 51.51;
+let lattitude1 = -0.1;
+
 let map = L.map('map-bloc').setView([longitude, lattitude], 13);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}{r}.png', {
+	attribution: '<a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia</a>',
+	minZoom: 2,
+	maxZoom: 15
 }).addTo(map);
-// L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     id: 'mapbox.streets',
-//     accessToken: 'your.mapbox.access.token'
-// }).addTo(map);
 
 
 L.marker([longitude, lattitude]).addTo(map)
     .bindPopup(`Position 1.<br> Lng. ${longitude} & Lat. ${lattitude}.`)
     .openPopup();
+
+
+L.marker([longitude1, lattitude1]).addTo(map)
+    .bindPopup(`Position 2.<br> Lng. ${longitude1} & Lat. ${lattitude1}.`)
+    .openPopup();
+
+// Onload, execute the following code
+$(document).ready(() => {
+
+    // Load JSON file
+    var json = require('./gps-coords.json');
+    $.getJSON("./gps-coord.json", function(json) {
+        console.log(json); // this will show the info it in firebug console
+    });
+    
+})
